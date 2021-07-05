14842c7b448f11ffd348bc33d1e0def60e4d1ac3
Kim Jong IL <nijin39@gmail.com>
2021-07-05 11:27:10 +0900

    unit test add
    - mvctest
    - mockito

--- src/generate-structure.js
+++ src/generate-structure.js
@@ -15,6 +15,9 @@ function generateComponent(packageName, aggregateName, aggregateUpperName, baseF
 
     let componentMap = new Map([
         ['controller', `${baseFolder}/${aggregateName}/interfaces/rest/${aggregateUpperName}RestController.java`],
+        ['annotation', `${baseFolder}/${aggregateName}/interfaces/rest/validation/Phone.java`],
+        ['validator', `${baseFolder}/${aggregateName}/interfaces/rest/validation/PhoneValidator.java`],
+        ['testController', `${baseFolder}/${aggregateName}/interfaces/rest/${aggregateUpperName}RestTestController.java`],
         ['commandservice', `${baseFolder}/${aggregateName}/application/internal/commandservices/${aggregateUpperName}CommandService.java`],
         ['queryservice', `${baseFolder}/${aggregateName}/application/internal/queryservices/${aggregateUpperName}QueryService.java`],
         ['assembler', `${baseFolder}/${aggregateName}/interfaces/rest/transform/Create${aggregateUpperName}CommandDTO${componentUpperName}.java`],
@@ -52,7 +55,7 @@ function generateComponent(packageName, aggregateName, aggregateUpperName, baseF
     });
 }
 
-async function generateFiles(packageName, aggregateName, baseFolder) {
+async function generateSourceFiles(packageName, aggregateName, baseFolder) {
 
     const aggregateUpperName = aggregateName.replace(/\w/, c => c.toUpperCase());
 
@@ -70,6 +73,8 @@ async function generateFiles(packageName, aggregateName, baseFolder) {
             generateComponent(packageName, aggregateName, aggregateUpperName, baseFolder, 'repository');
             generateComponent(packageName, aggregateName, aggregateUpperName, baseFolder, 'jparepository');
             generateComponent(packageName, aggregateName, aggregateUpperName, baseFolder, 'apiControllerAdvice');
+            generateComponent(packageName, aggregateName, aggregateUpperName, baseFolder, 'annotation');
+            generateComponent(packageName, aggregateName, aggregateUpperName, baseFolder, 'validator');
             resolve(true);
         } catch (e) {
             reject(e.message);
@@ -78,24 +83,58 @@ async function generateFiles(packageName, aggregateName, baseFolder) {
     });
 }
 
+async function generateTestFiles(packageName, aggregateName, baseFolder) {
+
+    const aggregateUpperName = aggregateName.replace(/\w/, c => c.toUpperCase());
+
+    return new Promise(function(resolve, reject) {
+        try {
+            console.log("Base :",baseFolder);
+            generateComponent(packageName, aggregateName, aggregateUpperName, baseFolder, 'testController');
+            resolve(true);
+        } catch (e) {
+            reject(e.message);
+        }
+
+    });
+}
+
+function createSoruceBase(packageName) {
+    makeFolder('./src/main');
+    makeFolder('./src/main/java');
+
+    let packageLayer = packageName.split('.');
+    let folderName = [];
+    packageLayer.forEach(packageName => {
+        folderName.push(packageName);
+        makeFolder(`./src/main/java/${folderName.join('/')}`)
+    });
+    return folderName;
+}
+
+function createTestBase(packageName) {
+    makeFolder('./src/test');
+    makeFolder('./src/test/java');
+
+    let packageLayer = packageName.split('.');
+    let folderName = [];
+    packageLayer.forEach(packageName => {
+        folderName.push(packageName);
+        makeFolder(`./src/test/java/${folderName.join('/')}`)
+    });
+    return folderName;
+}
+
 function createFolders(packageName, aggregateName) {
     return new Promise(function(resolve, reject) {
         try {
-            makeFolder('./src/main');
-            makeFolder('./src/main/java');
-
-            let packageLayer = packageName.split('.');
-            let folderName = [];
-            packageLayer.forEach(packageName => {
-                folderName.push(packageName);
-                makeFolder(`./src/main/java/${folderName.join('/')}`)
-            });
-
+            const folderName = createSoruceBase(packageName);
             makeFolder(`./src/main/java/${folderName.join('/')}/config`);
             makeFolder(`./src/main/java/${folderName.join('/')}/${aggregateName}`);
             makeFolder(`./src/main/java/${folderName.join('/')}/${aggregateName}/interfaces`);
             makeFolder(`./src/main/java/${folderName.join('/')}/${aggregateName}/interfaces/rest`);
             makeFolder(`./src/main/java/${folderName.join('/')}/${aggregateName}/interfaces/rest/transform`);
+            makeFolder(`./src/main/java/${folderName.join('/')}/${aggregateName}/interfaces/rest/validation`);
             makeFolder(`./src/main/java/${folderName.join('/')}/${aggregateName}/interfaces/rest/dto`);
             makeFolder(`./src/main/java/${folderName.join('/')}/${aggregateName}/interfaces/events`);
             makeFolder(`./src/main/java/${folderName.join('/')}/${aggregateName}/application`);
@@ -116,7 +155,12 @@ function createFolders(packageName, aggregateName) {
             makeFolder(`./src/main/java/${folderName.join('/')}/${aggregateName}/infrastructure/brokers/kafka`);
             makeFolder(`./src/main/java/${folderName.join('/')}/${aggregateName}/infrastructure/repositories`);
             makeFolder(`./src/main/java/${folderName.join('/')}/${aggregateName}/infrastructure/repositories/jpa`);
-            resolve(`./src/main/java/${folderName.join('/')}`);
+
+            const testFolderName = createTestBase(packageName);
+            makeFolder(`./src/test/java/${testFolderName.join('/')}/${aggregateName}`);
+            makeFolder(`./src/test/java/${testFolderName.join('/')}/${aggregateName}/interfaces`);
+            makeFolder(`./src/test/java/${testFolderName.join('/')}/${aggregateName}/interfaces/rest`);
+            resolve({ sourceBase: `./src/main/java/${folderName.join('/')}`, testBase: `./src/test/java/${testFolderName.join('/')}`});
         } catch (e) {
             reject(e.message);
         }
@@ -124,6 +168,7 @@ function createFolders(packageName, aggregateName) {
 }
 
 module.exports.generateStructure = async (aggregateName, packageName) => {
-    const baseFolder = await createFolders(packageName, aggregateName);
-    await generateFiles(packageName, aggregateName, baseFolder);
+    const { sourceBase, testBase } = await createFolders(packageName, aggregateName);
+    await generateSourceFiles(packageName, aggregateName, sourceBase);
+    await generateTestFiles(packageName, aggregateName, testBase);
 }
